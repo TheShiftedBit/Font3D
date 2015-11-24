e4b4a77e093be33c94cc22eda716ea491828b5ed
Bitshift <theshiftedbit@gmail.com>
2015-11-24 15:45:23 -0500

    Initital commit

---
+++ Assets/Font3D/Editor/Font3DEditor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+[CustomEditor(typeof(Font3D))]
+public class Font3DEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		//DrawDefaultInspector();
+		EditorGUILayout.HelpBox("Rendering Fixed!", MessageType.None);
+	}
+}
---
+++ Assets/Font3D/Font3D.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(MeshRenderer))]
+[RequireComponent (typeof(TextMesh))]
+#if UNITY_EDITOR
+[ExecuteInEditMode]
+#endif
+public class Font3D : MonoBehaviour {
+	Renderer rend;
+	TextMesh mesh;
+	Shader shader;
+	[SerializeField]
+	Material matInstance;
+
+	void Update() {
+		if(rend == null) {
+			rend = GetComponent<Renderer>();
+			if(rend == null) {
+				rend = gameObject.AddComponent<MeshRenderer>();
+			}
+		}
+		if(mesh == null) {
+			mesh = GetComponent<TextMesh>();
+			if(mesh == null) {
+				mesh = gameObject.AddComponent<TextMesh>();
+			}
+		}
+
+		if(matInstance == null) {
+			if(shader == null) {
+				shader = Shader.Find("3D Text");
+			}
+			matInstance = new Material(shader);
+			matInstance.hideFlags = HideFlags.HideInInspector;
+		}
+
+		if(rend.sharedMaterial != matInstance) {
+			rend.sharedMaterial = matInstance;
+		}
+
+		rend.sharedMaterial.color = mesh.color;
+		rend.sharedMaterial.mainTexture = mesh.font.material.mainTexture;
+	}
+
+	void OnDestroy() {
+		DestroyImmediate(matInstance);
+	}
+}
